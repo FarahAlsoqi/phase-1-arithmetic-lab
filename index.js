109e996993c91f201f2fa74388103c606508ea74
Farah alsoqi <farahmanna97@gmail.com>
2023-09-24 20:22:32 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let multiply;
+let num1 = 2;
+let num2 = 31;
+multiply = num1 * num2;
+
+let random = 10;
+Number.isInteger(random);
+
+let mod;
+let num3 = 22;
+let num4 = 6;
+mod = num3 % num4;
+
+let max = Math.max(7, 12, 20, 15, 3);
